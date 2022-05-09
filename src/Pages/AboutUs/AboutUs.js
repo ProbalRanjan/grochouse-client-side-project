d33c90e6227bd522ded16a27d0ed7fbd92299249
Probal Ranjan Paul <probal.ranjan1198@gmail.com>
2022-05-09 23:42:29 +0600

    final project done

--- src/Pages/AboutUs/AboutUs.js
+++ src/Pages/AboutUs/AboutUs.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import './AboutUs.css'
 
 const AboutUs = () => {
     return (
-        <div className='container my-5'>
+        <div className='container my-5 about-container blog'>
             <h1 style={{ textAlign: "center" }}>About</h1>
-            <img src="" alt="" />
-            <p></p>
+            <img src="https://i.ibb.co/wRG8Pk7/pexels-bence-szemerey-7513430.jpg" alt="" />
+            <p>A warehouse is a building for storing goods. Warehouses are used by manufacturers, importers, exporters, wholesalers, transport businesses, customs, etc. They are usually large plain buildings in industrial parks on the outskirts of cities, towns, or villages.
+
+                They usually have loading docks to load and unload goods from trucks. Sometimes warehouses are designed for the loading and unloading of goods directly from railways, airports, or seaports. They often have cranes and forklifts for moving goods, which are usually placed on ISO standard pallets and then loaded into pallet racks. Stored goods can include any raw materials, packing materials, spare parts, components, or finished goods associated with agriculture, manufacturing, and production. In India and Hong Kong, a warehouse may be referred to as a "godown". There are also godowns in the Shanghai Bund.</p>
         </div>
     );
 };
